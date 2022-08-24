754d152a6a7631c7b90c8437f8d668397fc5d304
Tushar Goyal <nayanamegha2000@gmail.com>
2022-08-24 13:20:46 +0530

    commit

---
+++ JavaDemos/src/com/tnsif/lesson3e/Five.java
@@ -0,0 +1,23 @@
+package com.tnsif.lesson3e;
+class DemoSix{
+	void displayOutput() {
+		System.out.println("Im a six")
+	}
+
+}
+public class Five {
+	class DemoFive{
+		void displayOutput() {
+			System.out.println("Im a six")
+		}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//we cannot acces using chaild class
+		
+		}
+
+	}
+
+}
---
+++ JavaDemos/src/com/tnsif/lesson3f/Six.java
@@ -0,0 +1,18 @@
+package com.tnsif.lesson3f;
+class DemoFive{
+	void displayOutput() {
+		System.out.println("Im a five")
+	}
+
+}
+public class Six {
+	void displayOutput() {
+		System.out.println("Im a five")
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+	}
+
+}
